7bc01508d885171884e867f3599051aa71ca01f5
drune <drune@4b0935fe-0800-0410-83af-d176843bc316>
2005-09-24 20:22:55 +0000

    maybe last changes before 0.3 - check Changelog

--- trunk/pyetun_focus.py
+++ trunk/pyetun_focus.py
@@ -51,3 +51,15 @@ def get_revert():
 def set_revert(value):
 	value=str(value)
 	os.popen(e_remote_path+" -focus-revert-on-hide-or-close-set "+value)
+
+
+def get_maximize():
+	maxi=os.popen(e_remote_path+" -maximize-policy-get").readlines()
+	del maxi[0]
+	maxi=maxi[:-1]
+	maxi=maxi[0].split(" ")[1][:-1] #python rocks
+	return maxi
+
+def set_maximize(value):
+	os.popen(e_remote_path+" -maximize-policy-set "+value)
+	
