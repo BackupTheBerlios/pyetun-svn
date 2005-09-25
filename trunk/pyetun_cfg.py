1693862bad3e94afc1c3954925cb7d4a3b3eca67
drune <drune@4b0935fe-0800-0410-83af-d176843bc316>
2005-09-25 15:38:51 +0000

    autoconf implemented, recursivity fixes, many other stuff..

--- trunk/pyetun_cfg.py
+++ trunk/pyetun_cfg.py
@@ -4,6 +4,7 @@ def return_cfg():
 	try:
 		import ConfigParser
 		config = ConfigParser.ConfigParser()
+		
 		if os.path.exists("/etc/pyetun.conf"):
 			config.read("/etc/pyetun.conf")
 			
@@ -32,3 +33,52 @@ def return_modpath(mod_type):
 	value=config.get("modules",mod_type)
 	return value
 
+#set enlightenment_remote path...to use with autogenerate
+def set_er_path():
+	config=return_cfg()
+
+	val=os.popen("whereis enlightenment_remote").readlines()
+	val2=val[0].split(" ")
+	# delete first element (name of app..)
+	del val2[0]
+	try:
+		f = open("/etc/pyetun.conf", 'w')
+
+	except IOError:
+		print "\n> Error: Permission Denied when editing /etc/pyetun.conf or File not Found!"
+		sys.exit()
+
+	for i in val2: 
+		if os.path.exists(i):
+			
+			config.set("paths","enlightement_remote", i)
+			config.write(f)
+			f.close()
+			break
+
+def set_modules_path():
+	
+	pos_paths=["/usr/lib/enlightenment/modules/","/usr/local/lib/enlightenment/modules/","/opt/lib/enlightenment/modules/"]
+	
+	config=return_cfg()
+	try:
+
+		f2 = open("/etc/pyetun.conf", 'w')
+	except IOError:
+		print "\n> Error: Permission Denied when editing /etc/pyetun.conf or File not Found!"
+		sys.exit()
+	for j in pos_paths:
+		if os.path.exists(j):
+			config.set("modules","normal", j)
+			
+			config.set("modules","extra",j[:-1]+"_extra/")
+			config.write(f2)
+			f2.close()
+			break
+
+
+			
+
+			
+
+	
--- trunk/setup.py
+++ trunk/setup.py
@@ -2,13 +2,12 @@
 
 from distutils.core import setup
 
+
 setup(name='pyetun',
-      version='0.2',
+      version='0.3',
       description='E17 tunner',
       author='Luis Marques',
       author_email='drune@gmx.net',
       url='http://pyetun.berlios.de',
       py_modules=['pyetun_bshade', 'pyetun_cache', 'pyetun_cfg', 'pyetun_desk', 'pyetun_focus', 'pyetun_font', 'pyetun_keys', 'pyetun_kill', 'pyetun_lang', 'pyetun_mod', 'pyetun_winlist', 'pyetun_edge'],
-		data_files=[('/etc', ['pyetun.conf']),
-					('/usr/bin', ['pyetun'])]
-     )
+		data_files=[('/etc', ['pyetun.conf']), ('/usr/bin', ['pyetun'])])
